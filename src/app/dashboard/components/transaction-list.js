b8cc17bdcd93f0775161a44939e11a2a9efd3508
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-05-22 19:13:36 +0300

    add pagination

--- src/app/dashboard/components/transaction-list.js
+++ src/app/dashboard/components/transaction-list.js
@@ -1,11 +1,32 @@
 "use client";
+import Button from "@/components/button";
 import Separator from "@/components/separator";
 import TransactionItem from "@/components/transaction-item";
 import TransactionSummary from "@/components/transaction-summary";
+import { fetchTransactions } from "@/lib/actions";
 import { transactionsByDate } from "@/lib/transactionsByDate";
+import { Loader } from "lucide-react";
+import { useState } from "react";
 
 const TransactionList = ({ initialTransactions, range }) => {
-  const grouped = transactionsByDate(initialTransactions);
+  const [transactions, setTransactions] = useState(initialTransactions);
+  const [buttonHidden, setButtonHidden] = useState(
+    initialTransactions.length === 0
+  );
+  const [loading, setLoading] = useState(false);
+  const grouped = transactionsByDate(transactions);
+
+  const handleClick = async () => {
+    setLoading(true);
+    let next = null;
+    try {
+      next = await fetchTransactions(range, transactions.length, 10);
+      setButtonHidden(next.length === 0);
+      setTransactions((prev) => [...prev, ...next]);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="space-y-8">
       {Object.entries(grouped).map(([date, { transactions, amount }]) => (
@@ -21,6 +42,21 @@ const TransactionList = ({ initialTransactions, range }) => {
           </section>
         </div>
       ))}
+      {transactions.length === 0 && (
+        <div className="text-center text-gray-400 dark:text-gray-500">
+          No transactions found
+        </div>
+      )}
+      {!buttonHidden && (
+        <div className="flex justify-center">
+          <Button variant="ghost" onClick={handleClick} disabled={loading}>
+            <div className="flex items-center space-x-1">
+              {loading && <Loader className="animate-spin" />}
+              <div>Load More</div>
+            </div>
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
